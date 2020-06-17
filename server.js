565153c95c910c9e2298ab652a0587bc72c61241
Silva404 <brenosilva0252@gmail.com>
2020-06-17 16:18:21 -0300

    iniciar o backend

---
+++ server.js
@@ -0,0 +1,36 @@
+const express = require('express')
+const nunjucks = require('nunjucks')
+
+const server = express()
+
+// usar o arquivo njk e não especificar ele
+server.set('view engine', 'njk')
+
+// pega apasta public e deixa na home
+server.use(express.static('public'))
+
+
+// atrelando o servidor express com o nunjucks
+nunjucks.configure('views', {
+    express: server,
+    noCache: true
+})
+
+
+server.get('/', (req, res) => {
+    res.render('home')
+})
+
+
+server.get('/about', (req, res) => {
+    res.render('about')
+})
+
+server.get('/portfolio', (req, res) => {
+    res.render('portfolio')
+})
+
+
+server.listen(3000, () => {
+    console.log('Servidor ligado!')
+})
